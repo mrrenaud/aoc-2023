d35be311a655473dcc0a4e56182758363b5480e4
Renaud Dumont <renaud@sparkle.tech>
2023-12-02 17:07:38 +0100

    added day02 - part 1

--- index.ts
+++ index.ts
@@ -1,2 +1,3 @@
 import "./src/day01-01";
 import "./src/day01-02";
+import "./src/day02";
---
+++ src/day02-01/index.ts
@@ -0,0 +1,36 @@
+import { CubeType, Input, input, testInput, testOutput } from "./values";
+
+const main = (input: Input): number => {
+  const games = input.games.split("\n");
+
+  const possible = games.reduce((acc, game) => {
+    const gameNumber = Number(game.match(/\d+/g)![0]);
+
+    const sets = game.split(":")[1].split(";");
+
+    const impossibleSet = sets.find((set) => {
+      const impossibleCube = set.split(",").find((cube) => {
+        const data = cube.trim().split(" ");
+        return input.conditions[data[1] as CubeType] < parseInt(data[0]);
+      });
+      return impossibleCube;
+    });
+
+    if (!impossibleSet) {
+      return acc + gameNumber;
+    }
+    return acc;
+  }, 0);
+
+  return possible;
+};
+
+console.log("Day 02 : How much possible ?");
+if (main(testInput) !== testOutput) {
+  throw new Error("Test failed");
+} else {
+  console.log("Test passed");
+}
+
+const answer = main(input);
+console.log("Answer", answer);
